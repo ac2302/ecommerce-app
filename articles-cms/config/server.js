26351631aa0e616bc9691c4c7ae226b35c5eb849
Aryan Chavan <aryan@aryansdomain.com>
2021-07-18 11:43:01 +0530

    add basic strapi cms

---
+++ articles-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '549d2af7594bea2f9338600dff342966'),
+    },
+  },
+});
---
+++ articles-cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eedfde35-b09e-41e0-8b7e-61c6a3107c8b'
+};
