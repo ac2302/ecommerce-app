c1c4607ae5b4d02f504f3f0ce04bad962fc0d12a
Aryan Chavan <aryan@aryansdomain.com>
2021-07-18 16:04:20 +0530

    add basic middlewares and connection to db

--- auth-api/index.js
+++ auth-api/index.js
@@ -2,8 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// connecting to db
+mongoose.connect(process.env.DB_STRING, (err) => {
+	if (err) console.error(err);
+	else console.log("connected to db");
+});
+
+// middlewares
+app.use(express.json());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 const PORT = process.env.PORT || 8085;
 app.listen(PORT, () => console.log("listening on port " + PORT));
