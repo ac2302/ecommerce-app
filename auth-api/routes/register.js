203779a52c1b23cc0f7f0632f496b1e936160aa8
Aryan Chavan <aryan@aryansdomain.com>
2021-07-18 16:35:29 +0530

    add user registration

--- auth-api/routes/register.js
+++ auth-api/routes/register.js
@@ -1,8 +1,34 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 router.post("/", async (req, res) => {
-	res.json({ hello: "there" });
+	const { firstName, lastName, email, password } = req.body;
+	if (!firstName || !lastName || !email || !password) {
+		// invalid request
+		res.status(400).json({
+			message: "body must contain firstName, lastName, email and password",
+		});
+		return;
+	}
+
+	try {
+		// salting and hashing password
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		// creating new user and saving to database
+		const newUser = await new User({
+			firstName,
+			lastName,
+			email,
+			password: hashedPassword,
+		});
+		await newUser.save();
+		res.json({ newUser });
+	} catch (err) {
+		res.status(500).json({ err });
+	}
 });
 
 module.exports = router;
